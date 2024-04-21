76e745e28c564bce382c19e664c8eb1ed8218563
rapeti-saikumar <rapetisaikumar9866@gmail.com>
2024-04-21 15:27:19 +0530

    first commit

---
+++ 16_useReg_exp2.jsx
@@ -0,0 +1,21 @@
+import React ,{useEffect, useRef, useState}from 'react'
+// accessing the DOM elements using useRef hook
+function App() {
+  const [text,setText] = useState("");
+  const userDom = useRef('');
+
+  const focus =()=>
+  {
+    userDom.current.focus();
+  }
+ 
+  return (
+    <div>
+      <input type="text" onChange={(e)=>setText(e.target.value)} ref={userDom}></input>
+      <h4>{text}</h4>
+      <button onClick={focus}>click</button>
+    </div>
+  )
+}
+
+export default App
